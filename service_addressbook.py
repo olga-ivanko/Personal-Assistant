3ad93b7234c345ccced87bfd064993fd9b8516db
olga-ivanko <o.ivanko@me.com>
2023-11-12 20:50:48 +0200

    func edit record added

--- service_addressbook.py
+++ service_addressbook.py
@@ -34,7 +34,6 @@ def func_normalize_phone(phone):
         .replace("-", "")
         .replace(" ", "")
     )
-
     return new_phone
 
 
@@ -93,6 +92,49 @@ def func_add(*args):
     return f"Record {rec_id = }, {new_phone = } added"
 
 
+def func_edit_record(*args):
+    rec_id = args[0].lower()
+    if not rec_id in book.keys():
+        return f"Record do not exist"
+
+    record = book.find(rec_id)
+    print(record)
+    print(f"type what needs to be changed:\n phone \n email\n address \n birthday")
+    user_input = input("").lower()
+    if user_input == "phone":
+        print(
+            "type next command:\n add phone <new number>\n change <old number> <new number>"
+        )
+        user_input2 = input("").lower().split()
+        if user_input2[0] == "change":
+            record.edit_phone(user_input2[1], user_input2[2])
+            return f"Record updated as:\n {record}"
+        elif " ".join(user_input2[:2]) == "add phone":
+            record.add_phone(user_input2[2])
+            return f"Record updated as:\n {record}"
+        else:
+            return unknown()
+    elif user_input == "email":
+        if record.email.value != "no email":
+            print(f"current email is {record.email.value}.")
+        user_input2 = input("Print new email:\n")
+        record.email.value = user_input2.strip()
+        return f"Record updated as:\n {record}"
+    elif user_input == "address":
+        print(f"current addess is {record.address.value}.")
+        user_input2 = input("Print new address:\n")
+        record.address.value = user_input2.strip()
+        return f"Record updated as:\n {record}"
+    elif user_input == "birthday":
+        if record.birthday.value:
+            print(f"current addess is {record.birthday.value}.")
+            user_input2 = input("Print new birthday:\n")
+            record.a
+            return f"Record updated as:\n {record}"
+    else:
+        return unknown()
+
+
 @user_error
 def add_birthday(*args):
     rec_id = args[0]
@@ -137,13 +179,13 @@ def func_change(*args):
         return f"Check the phone: {args[2]}. Wrong format."
 
     book.find(rec_id).edit_phone(old_phone, new_phone)
-    return f"Record {rec_id} is updated with nes phone: {book.get(rec_id).phones[0]}"
+    return f"Record {rec_id} is updated with new phone: {new_phone}"
 
 
-@user_error
-def func_phone(*args):
-    rec_id = args[0]
-    return f"Phone of {rec_id} is {book.get(rec_id).phones[0]}"
+# @user_error
+# def func_phone(*args):
+#    rec_id = args[0]
+#    return f"Phone(s) of {rec_id} is {book.get(rec_id).phones[0]}"
 
 
 def func_hello(*args):
@@ -172,20 +214,21 @@ def func_show(*args):
 
 
 @user_error
-def func_find(*args):
-    line = ""
-    for record in book.values():
-        str_rec = str(record.name)
-        for ph in record.phones:
-            str_rec += str(ph)
-        found_rec = re.findall(args[0], str_rec)
-        if len(found_rec) != 0:
-            line += f"{record}\n"
-
-    if len(line) == 0:
-        return f'the search for key "{args[0]}" gave no results. Try other key.'
-    print(f'result for "{args[0]}" search:')
-    return line
+def func_find(args):
+    if len(args) > 2:
+        line = ""
+        for record in book.values():
+            str_rec = str(record.name)
+            for ph in record.phones:
+                str_rec += str(ph)
+            found_rec = re.findall(args[0], str_rec)
+            if len(found_rec) != 0:
+                line += f"{record}\n"
+        if len(line) == 0:
+            return f'the search for key "{args[0]}" gave no results. Try other key.'
+        print(f'result for "{args[0]}" search:')
+        return line
+    return "Please enter 3 or more symbols for search"
 
 
 @user_error
@@ -207,8 +250,9 @@ def func_sort_folder(*args):
 FUNCTIONS = {
     "hello": func_hello,
     "add": func_add,
+    "edit record": func_edit_record,
     "change": func_change,
-    "phone": func_phone,
+    # "phone": func_phone,
     "show all": func_show_all,
     "show": func_show,
     "find": func_find,
